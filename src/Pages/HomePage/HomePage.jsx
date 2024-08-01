cd6b5362cbcbe2caa030a9fadb1a73aeb4b3b155
Harshil Gupta <53968071+Harshil-Gupta@users.noreply.github.com>
2024-08-01 19:38:38 +0530

    Code Push

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import "./HomePage.css";
+import React from "react";
+import InvestmentChoice from "../../components/InvestmentChoice/InvestmentChoice";
+
+const HomePage = () => {
+  return (
+    <div className="HomePage">
+      <InvestmentChoice />
+    </div>
+  );
+};
+
+export default HomePage;
